f0611344859247ef40ae33faf9ec1b45fb846be9
Mo9ses <mercydelmas@outlook.com>
2025-11-27 20:34:15 -0500

    A couple changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,17 +20,15 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px] mx-auto text-center'
       >
-        Hello! My name is Bryan Athanas and I'm a skilled future cloud engineer with
-        experience in TypeScript, JavaScript, Java, Python, and many more languages. I am a quick learner
-        and I want to create efficient, scalable, and user-friendly solutions in the future.
+        Hello. My name is Bryan Athanas and I'm a skilled aspiring cloud engineer with experience in TypeScript, JavaScript, Java, and Python. My goal is to create efficient, scalable, and user-friendly solutions in the future.
       </motion.p>
-      <div className='mt-16 flex flex-row flex-wrap justify-center gap-10'>
+      {/* <div className='mt-16 flex flex-row flex-wrap justify-center gap-10'>
         {technologies.map((technology) => (
           <div className='w-28 h-28' key={technology.name}>
             <BallCanvas icon={technology.icon} />
           </div>
         ))}
-      </div>
+      </div> */}
     </>
   );
 };
